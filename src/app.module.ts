6b1403096de15cb3c1e77af449553bb29706372e
MaxHuang <hc18174419502@163.com>
2025-02-24 11:27:12 +0800

    update 0224

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { DbModule } from './db/db.module';
-import { BookModule } from './book/book.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UserModule} from './user/user.module';
+import {DbModule} from './db/db.module';
+import {BookModule} from './book/book.module';
+import {FileModule} from './file/file.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, DbModule, BookModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UserModule, DbModule, BookModule, FileModule, TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'xzy20000219',
+        database: 'login_test',
+        // synchronize: true
+    })],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {Controller, Post, Body, UseInterceptors, UploadedFiles} from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
-import {LoginUserDto} from "./dto/login-user.dto";
+import { LoginUserDto } from "./dto/login-user.dto";
+import {FilesInterceptor} from "@nestjs/platform-express";
+import {Express} from "express";
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post('register')
   register(@Body() registerUserDto: RegisterUserDto) {
@@ -14,7 +16,16 @@ export class UserController {
   }
 
   @Post('login')
-  async login(@Body() loginUserDto: LoginUserDto){
+  async login(@Body() loginUserDto: LoginUserDto) {
     return this.userService.login(loginUserDto)
+  }
+
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('files', 20, {
+    dest: 'uploads1'
+  }))
+  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>, @Body() body) {
+    console.log('body', body)
+    console.log('files', files)
   }
 }
