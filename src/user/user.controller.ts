8673f30167e24df7a38d08cd6e27d3a27dadc475
MaxHuang <hc18174419502@163.com>
2025-02-24 14:26:02 +0800

    upload api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,6 +4,7 @@ import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from "./dto/login-user.dto";
 import {FilesInterceptor} from "@nestjs/platform-express";
 import {Express} from "express";
+import * as fs from "fs";
 
 @Controller('user')
 export class UserController {
@@ -20,12 +21,21 @@ export class UserController {
     return this.userService.login(loginUserDto)
   }
 
+  // 上传接口
   @Post('upload')
   @UseInterceptors(FilesInterceptor('files', 20, {
     dest: 'uploads1'
   }))
-  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>, @Body() body) {
+  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>, @Body() body: {name: string}) {
     console.log('body', body)
     console.log('files', files)
+    const fileName = body.name.match(/(.+)\-\d+$/)[1];
+    const chunkDir = 'uploads1/chunks_' + fileName;
+    if(!fs.existsSync(chunkDir)){
+      fs.mkdirSync(chunkDir);
+    }
+    fs.cpSync(files[0].path, chunkDir + '/' + body.name);
+    fs.rmSync(files[0].path);
   }
+
 }
